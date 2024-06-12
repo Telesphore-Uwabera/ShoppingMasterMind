dc7d85f028706ffcbae7943f4e7ced6da3ba50b1
TelesphoreUwabera <t.uwabera@alustudent.com>
2024-06-12 22:20:52 +0200

    :

--- ShoppingMasterMind/lib/screens/welcome_screen.dart
+++ ShoppingMasterMind/lib/screens/welcome_screen.dart
@@ -1,4 +1,3 @@
-// lib/screens/welcome_screen.dart
 import 'package:flutter/material.dart';
 
 class WelcomeScreen extends StatelessWidget {
@@ -13,23 +12,61 @@ class WelcomeScreen extends StatelessWidget {
               borderRadius: BorderRadius.circular(20.0), // Set the border radius here
               child: Image.asset('assets/shopping_image.png'),
             ), // Replace with your image asset
+            SizedBox(height: 20), // Add space between the image and the text
             Text(
               'Shopping Mastermind',
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold, color: Colors.blue,),
+              style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+                color: Colors.blue,
+              ),
             ),
             Text(
               'Take control and achieve your shopping goals.',
               textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 20),
             ),
             SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/login');
-              },
-              child: Text("Let's Get Started"),
+            ClipRRect(
+              borderRadius: BorderRadius.circular(20.0), // Set the border radius here
+              child: Container(
+                color: Colors.blue,
+                padding: EdgeInsets.all(16.0),
+                child: Text(
+                  "Let's Get Started",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20.0, // Adjust the font size as needed
+                  ),
+                ),
+              ),
             ),
           ],
         ),
+      ),
+    );
+  }
+}
+
+void main() {
+  runApp(MaterialApp(
+    home: WelcomeScreen(),
+    routes: {
+      '/login': (context) => LoginScreen(), // Make sure to define this screen
+    },
+  ));
+}
+
+// Add this placeholder for the LoginScreen if you haven't already defined it.
+class LoginScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Login Screen'),
+      ),
+      body: Center(
+        child: Text('Login Screen Content'),
       ),
     );
   }
