74e8684eaf899201116c45380c4e5dc11ec94798
TelesphoreUwabera <t.uwabera@alustudent.com>
2024-06-14 10:57:59 +0200

    Added bin folder and App Icons and Splash Screens

---
+++ bin/shopping_mastermind.dart
@@ -0,0 +1,8 @@
+// Example content of bin/shopping_mastermind.dart
+
+void main() {
+  print('Welcome to Shopping MasterMind!');
+  // Additional initialization or setup code can go here
+}
+
+// Additional Dart code related to your application can be defined here
