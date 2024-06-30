f52e84bbbc004c66b39e276e74c3f817d76c6785
TelesphoreUwabera <t.uwabera@alustudent.com>
2024-06-30 22:07:11 +0200

    Update schedule_task.dart

--- lib/screens/schedule_task.dart
+++ lib/screens/schedule_task.dart
@@ -1,101 +1,52 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Calendar App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: ScheduleTask(title: 'Calendar App'),
-    );
-  }
-}
-
 class ScheduleTask extends StatefulWidget {
   ScheduleTask({Key? key, required this.title}) : super(key: key);
 
   final String title;
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _ScheduleTaskState createState() => _ScheduleTaskState();
 }
 
-class _MyHomePageState extends State<ScheduleTask> {
-  int _selectedDay = 7;
-  DateTime _selectedDate = DateTime(2024, 6, 7);
+class _ScheduleTaskState extends State<ScheduleTask> {
   List<Event> _events = [
-    Event(date: DateTime(2024, 6, 7), time: '10:00AM', title: 'Buying Fruits'),
-    Event(date: DateTime(2024, 6, 7), time: '12:00PM', title: 'Buying Clothes'),
+    Event(date: DateTime(2024, 6, 7), time: TimeOfDay(hour: 10, minute: 0), title: 'Buying Fruits', items: 1),
+    Event(date: DateTime(2024, 6, 7), time: TimeOfDay(hour: 12, minute: 0), title: 'Buying Clothes', items: 2),
+    // Add more events as needed
   ];
 
-  void _selectDay(int day) {
+  void _addEvent(String title, int items, DateTime date, TimeOfDay time) {
     setState(() {
-      _selectedDay = day;
-      _selectedDate = DateTime(_selectedDate.year, _selectedDate.month, day);
+      _events.add(Event(date: date, time: time, title: title, items: items));
     });
   }
 
-  void _showDatePicker() {
-    showDatePicker(
-      context: context,
-      initialDate: _selectedDate,
-      firstDate: DateTime(2024),
-      lastDate: DateTime(2025),
-    ).then((pickedDate) {
-      if (pickedDate != null) {
-        setState(() {
-          _selectedDate = pickedDate;
-          _selectedDay = pickedDate.day;
-        });
-      }
+  void _removeEvent(Event event) {
+    setState(() {
+      _events.remove(event);
     });
   }
 
-  void _addEvent() {
+  void _showConfirmationDialog() {
     showDialog(
       context: context,
       builder: (BuildContext context) {
-        TextEditingController titleController = TextEditingController();
-        TextEditingController timeController = TextEditingController();
         return AlertDialog(
-          title: Text('Add Event'),
-          content: Column(
-            mainAxisSize: MainAxisSize.min,
+          title: Row(
             children: [
-              TextField(
-                controller: titleController,
-                decoration: InputDecoration(hintText: 'Enter event title'),
-              ),
-              SizedBox(height: 16),
-              TextField(
-                controller: timeController,
-                decoration: InputDecoration(hintText: 'Enter event time'),
-              ),
+              Icon(Icons.check_circle, color: Colors.green),
+              SizedBox(width: 10),
+              Text('Success'),
             ],
           ),
+          content: Text('Your command is successfully submitted!'),
           actions: [
             TextButton(
-              onPressed: () => Navigator.of(context).pop(),
-              child: Text('Cancel'),
-            ),
-            TextButton(
               onPressed: () {
-                setState(() {
-                  _events.add(Event(
-                    date: _selectedDate,
-                    time: timeController.text,
-                    title: titleController.text,
-                  ));
-                });
                 Navigator.of(context).pop();
               },
-              child: Text('Add'),
+              child: Text('OK'),
             ),
           ],
         );
@@ -106,159 +57,123 @@ class _MyHomePageState extends State<ScheduleTask> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
       appBar: AppBar(
         title: Text(widget.title),
-        backgroundColor: Colors.white,
-        centerTitle: true,
       ),
-      body: Column(
-        children: [
-          SizedBox(
-            height: 300,
-            child: CalendarView(
-              selectedDay: _selectedDay,
-              onDaySelected: _selectDay,
-              selectedDate: _selectedDate,
+      body: ListView.builder(
+        itemCount: _events.length,
+        itemBuilder: (context, index) {
+          final event = _events[index];
+          return ListTile(
+            title: Text(event.title),
+            subtitle: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text('Items: ${event.items}'),
+                Text('Time: ${event.time.format(context)}'),
+                Text('Date: ${event.date.toString()}'),
+              ],
             ),
-          ),
-          Expanded(
-            child: ListView.builder(
-              itemCount: _events
-                  .where((event) =>
-                      event.date.year == _selectedDate.year &&
-                      event.date.month == _selectedDate.month &&
-                      event.date.day == _selectedDate.day)
-                  .toList()
-                  .length,
-              itemBuilder: (context, index) {
-                final event = _events
-                    .where((event) =>
-                        event.date.year == _selectedDate.year &&
-                        event.date.month == _selectedDate.month &&
-                        event.date.day == _selectedDate.day)
-                    .toList()[index];
-                return Padding(
-                  padding: EdgeInsets.symmetric(vertical: 30.0, horizontal: 16.0),
-                  child: Container(
-                    color: Color.fromARGB(32, 20, 150, 243),
-                    child: ListTile(
-                      title: Text(event.title),
-                      subtitle: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(event.time),
-                        ],
-                      ),
-                      trailing: IconButton(
-                        icon: Icon(Icons.delete),
-                        onPressed: () {
-                          setState(() {
-                            _events.remove(event);
-                          });
-                        },
-                      ),
-                    ),
-                  ),
-                );
+            trailing: IconButton(
+              icon: Icon(Icons.delete),
+              onPressed: () {
+                _removeEvent(event);
               },
             ),
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          _showAddEventDialog(context);
+        },
+        child: Icon(Icons.add),
+        backgroundColor: Colors.blue,
+      ),
+      persistentFooterButtons: [
+        ElevatedButton(
+          onPressed: () {
+            _showConfirmationDialog();
+          },
+          child: Text('BUY'),
+          style: ButtonStyle(
+            backgroundColor: MaterialStateProperty.all<Color>(Colors.blue),
           ),
-
-        ],
-      ),
-      floatingActionButton: Container(
-        padding: EdgeInsets.only(bottom: 100),
-        child: FloatingActionButton(
-          onPressed: () {},
-          child: Text(
-            '+',
-            style: TextStyle(color: Colors.white, fontSize: 30.0, fontWeight: FontWeight.bold),
-          ),
-          backgroundColor: Colors.blue,
         ),
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.endContained,
-      bottomNavigationBar: Container(
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(20),
-          color: Colors.white,
-        ),
-        child: BottomNavigationBar(
-          type: BottomNavigationBarType.fixed,
-          iconSize: 40.0,
-          items: [
-            BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
-            BottomNavigationBarItem(icon: Icon(Icons.calendar_month_rounded), label: 'Calendar'),
-            BottomNavigationBarItem(icon: Icon(Icons.note_add_rounded), label: 'Notes'),
-            BottomNavigationBarItem(icon: Icon(Icons.settings), label: 'Settings'),
-          ],
-        ),
-      ),
+      ],
     );
   }
-}
 
-class CalendarView extends StatelessWidget {
-  final int selectedDay;
-  final Function(int) onDaySelected;
-  final DateTime selectedDate;
+  void _showAddEventDialog(BuildContext context) {
+    TextEditingController titleController = TextEditingController();
+    TextEditingController itemsController = TextEditingController();
+    TimeOfDay? selectedTime;
 
-  const CalendarView({
-    Key? key,
-    required this.selectedDay,
-    required this.onDaySelected,
-    required this.selectedDate,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return GridView.count(
-      crossAxisCount: 7,
-      shrinkWrap: true,
-      physics: NeverScrollableScrollPhysics(),
-      children: List.generate(31, (index) {
-        int day = index + 1;
-        return GestureDetector(
-          onTap: () {
-            onDaySelected(day);
-          },
-          child: Container(
-            decoration: BoxDecoration(
-              color: day == selectedDay
-                  ? Colors.blue.withOpacity(0.2)
-                  : Colors.white,
-              borderRadius: BorderRadius.circular(8),
-              border: Border.all(
-                color: day == selectedDay
-                    ? Colors.blue
-                    : Colors.grey.withOpacity(0.5),
-              ),
-            ),
-            alignment: Alignment.center,
-            child: Text(
-              '$day',
-              style: TextStyle(
-                fontSize: 18,
-                fontWeight: day == selectedDay ? FontWeight.bold : FontWeight.normal,
-                color: day == selectedDay ? Colors.blue : Colors.black,
-              ),
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Add Event'),
+          content: SingleChildScrollView(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                TextField(
+                  controller: titleController,
+                  decoration: InputDecoration(labelText: 'Product Name'),
+                ),
+                TextField(
+                  controller: itemsController,
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(labelText: 'Number of Items'),
+                ),
+                ListTile(
+                  title: Text('Time to Buy'),
+                  trailing: IconButton(
+                    icon: Icon(Icons.access_time),
+                    onPressed: () async {
+                      selectedTime = await showTimePicker(
+                        context: context,
+                        initialTime: TimeOfDay.now(),
+                      );
+                    },
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    if (titleController.text.isNotEmpty &&
+                        itemsController.text.isNotEmpty &&
+                        selectedTime != null) {
+                      _addEvent(
+                        titleController.text,
+                        int.parse(itemsController.text),
+                        DateTime.now(), // Change this to selected date
+                        selectedTime!,
+                      );
+                      Navigator.of(context).pop();
+                    }
+                  },
+                  child: Text('Add'),
+                ),
+              ],
             ),
           ),
         );
-      }),
+      },
     );
   }
 }
 
 class Event {
   final DateTime date;
-  final String time;
+  final TimeOfDay time;
   final String title;
+  final int items;
 
   Event({
     required this.date,
     required this.time,
     required this.title,
+    required this.items,
   });
 }
