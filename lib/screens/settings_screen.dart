1deb61e8a21af6c3fa0664650c3a906f71f0a3cf
TelesphoreUwabera <t.uwabera@alustudent.com>
2024-06-30 22:06:41 +0200

    Update settings_screen.dart

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -1,142 +1,111 @@
+// settings_screen.dart
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(SettingsApp());
-}
-
-class SettingsApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Settings Page',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: SettingsPage(),
-    );
-  }
-}
-
-class SettingsPage extends StatelessWidget {
+class SettingsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Settings'),
       ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              SettingsSection(
-                title: 'Account',
-                children: [
-                  SettingsTile(
-                    icon: Icons.person,
-                    title: 'Profile',
-                    subtitle: 'Update your profile information',
-                  ),
-                  SettingsTile(
-                    icon: Icons.lock,
-                    title: 'Change Password',
-                    subtitle: 'Update your password',
-                  ),
-                ],
-              ),
-              SettingsSection(
-                title: 'Notifications',
-                children: [
-                  SettingsTile(
-                    icon: Icons.notifications,
-                    title: 'Push Notifications',
-                    subtitle: 'Manage push notifications',
-                  ),
-                ],
-              ),
-              SettingsSection(
-                title: 'Privacy',
-                children: [
-                  SettingsTile(
-                    icon: Icons.lock_outline,
-                    title: 'Privacy Policy',
-                    subtitle: 'Read our privacy policy',
-                  ),
-                  SettingsTile(
-                    icon: Icons.security,
-                    title: 'Security',
-                    subtitle: 'Update security settings',
-                  ),
-                ],
-              ),
-              SettingsSection(
-                title: 'About',
-                children: [
-                  SettingsTile(
-                    icon: Icons.info,
-                    title: 'About Us',
-                    subtitle: 'Learn more about us',
-                  ),
-                  SettingsTile(
-                    icon: Icons.help,
-                    title: 'Help',
-                    subtitle: 'Get help and support',
-                  ),
-                ],
-              ),
-            ],
+      body: ListView(
+        children: [
+          ListTile(
+            title: Text('Username'),
+            onTap: () {
+              // Change username
+            },
           ),
-        ),
+          ListTile(
+            title: Text('Email'),
+            onTap: () {
+              // Change email
+            },
+          ),
+          ListTile(
+            title: Text('Password'),
+            onTap: () {
+              // Change password
+            },
+          ),
+          ListTile(
+            title: Text('Location Settings'),
+            onTap: () {
+              Navigator.pushNamed(context, '/faq'); // Replace with your location settings screen route
+            },
+          ),
+          ListTile(
+            title: Text('Deactivate Account'),
+            onTap: () {
+              showDialog(
+                context: context,
+                builder: (BuildContext context) {
+                  return AlertDialog(
+                    title: Text('Do you need to deactivate the account?'),
+                    actions: [
+                      TextButton(
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                        child: Text(
+                          'No',
+                          style: TextStyle(color: Colors.red),
+                        ),
+                      ),
+                      TextButton(
+                        onPressed: () {
+                          Navigator.pop(context);
+                          showDialog(
+                            context: context,
+                            builder: (BuildContext context) {
+                              return AlertDialog(
+                                title: Text('Account deactivated'),
+                                actions: [
+                                  TextButton(
+                                    onPressed: () {
+                                      Navigator.pop(context);
+                                    },
+                                    child: Text('OK'),
+                                  ),
+                                ],
+                              );
+                            },
+                          );
+                        },
+                        child: Text(
+                          'Yes',
+                          style: TextStyle(color: Colors.blue),
+                        ),
+                      ),
+                    ],
+                  );
+                },
+              );
+            },
+          ),
+        ],
       ),
-    );
-  }
-}
-
-class SettingsSection extends StatelessWidget {
-  final String title;
-  final List<Widget> children;
-
-  const SettingsSection({required this.title, required this.children});
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Padding(
-          padding: const EdgeInsets.symmetric(vertical: 16.0),
-          child: Text(
-            title,
-            style: Theme.of(context).textTheme.headlineSmall,
-          ),
-        ),
-        Column(children: children),
-      ],
-    );
-  }
-}
-
-class SettingsTile extends StatelessWidget {
-  final IconData icon;
-  final String title;
-  final String subtitle;
-
-  const SettingsTile({
-    required this.icon,
-    required this.title,
-    required this.subtitle,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return ListTile(
-      leading: Icon(icon),
-      title: Text(title),
-      subtitle: Text(subtitle),
-      onTap: () {
-        // Handle tile tap
-      },
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showDialog(
+            context: context,
+            builder: (BuildContext context) {
+              return AlertDialog(
+                title: Text('Profile updated!!!'),
+                actions: [
+                  TextButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: Text('OK'),
+                  ),
+                ],
+              );
+            },
+          );
+        },
+        child: Icon(Icons.check),
+      ),
     );
   }
 }
